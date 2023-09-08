bc20f5a12c301d714181e51500c04701a1152b96
ps <przemek.7678@gmail.com>
2023-09-08 18:09:49 +0200

    mulithreading

---
+++ GetData/get_data.py
@@ -0,0 +1,25 @@
+#  Code used in simulating the data. Repository used in creating this file:
+#  https://github.com/DanielJCase/Ising-Deep-Learning/blob/master/Ising_genData.ipynb
+import click
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
+from simulate import simulate
+from multiprocessing import Pool
+
+@click.command()
+@click.argument('steps', type=int)
+@click.argument('n_samples', type=int)
+@click.argument('output_file', type=str, default="Data")
+@click.argument('temp', type=float, default=2.9)
+@click.argument('H', type=float, default=0.)
+@click.argument('L', type=int, default=64.)
+@click.argument('n_workers', type=int, default=5)
+def get_data(steps, n_samples, output_file, temp, h, l, n_workers):
+    output_file = Path(output_file)
+    output_file.mkdir(parents=True, exist_ok=True)
+    with Pool(n_workers) as pool:
+        pool.starmap(simulate, [(steps, l, temp , h, output_file / f"out-{i}")  for i in range(n_samples)])
+
+if __name__ == '__main__':
+    get_data()
