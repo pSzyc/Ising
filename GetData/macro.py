40bd423526bdc23427d21415bfc87915d23730d4
Przemek <przemek.7678@gmail.com>
2023-09-24 11:12:37 +0200

    Notebook looks better

--- GetData/macro.py
+++ GetData/macro.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 def __get_neighbour_sum_matrix(mat):
     '''Matrix of sum of spin values for all neighbouring cells'''
@@ -12,6 +13,24 @@ def __get_neighbour_sum_matrix(mat):
     d_l = ((1, -1), (0, 1))
     shift_axis_list = [right, left, u_r, d_r, u_l, d_l, up, down]
     return sum([np.roll(mat, shift=shift_axis[0], axis=shift_axis[1]) for shift_axis in shift_axis_list])
+
+def __get_neighb_filter(shape):
+    kernel = np.array([[1.0, 1.0, 1.0], 
+       [1.0, 0.0, 1.0], 
+       [1.0, 1.0, 1.0]]) 
+    conv = tf.keras.layers.Conv2D(1, (3, 3), 
+        activation='linear',
+        input_shape=shape,
+        padding="same", 
+        kernel_initializer = tf.keras.initializers.Constant(kernel), use_bias=False
+    ) 
+    return conv
+
+def get_neighbour_sum_matrix_conv(mat):
+    conv_filter = __get_neighb_filter(mat.shape)
+    sum_mat = conv_filter(mat.reshape(1,mat.shape[0],mat.shape[1], 1).astype(np.float32))
+    return sum_mat[0,:,:,0]
+
 
 def calcEnergy(mat):
     '''Energy of a given configuration'''
