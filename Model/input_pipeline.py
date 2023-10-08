af4323cb44cd9136387de92e7d29d0c520ba2b1d
pSzyc <przemek.7678@gmail.com>
2023-10-08 10:09:05 +0200

    models moved to Model folder

--- Model/input_pipeline.py
+++ Model/input_pipeline.py
@@ -8,13 +8,17 @@ def load_npy_file(file_path):
 def load_numpy_file_wrapper(file_path):
     return tf.numpy_function(load_npy_file, [file_path], tf.int32)
 
-def dataset_pipeline(path):
+def dataset_pipeline(path, flatten=True, epochs=1, batch_size=1):
     print("Getting data from " + path)
     dataset = tf.data.Dataset.list_files(f"{path}/*/*.npy")
     print(f"Got {len(dataset)} samples")
     dataset = dataset.map(load_numpy_file_wrapper)
-    dataset = dataset.map(lambda x: tf.reshape(x, [-1]))
+    if flatten:
+        dataset = dataset.map(lambda x: tf.reshape(x, [-1]))
+    else:
+        dataset = dataset.map(lambda x: tf.expand_dims(x, 2))
     dataset = dataset.map(lambda x: tf.cast(x, tf.float32))
+    dataset = dataset.repeat(epochs).batch(batch_size)
     return dataset
 
 def get_param_dict(path):
