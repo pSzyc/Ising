832647e32aa37875949ca8695e2137f4b1cdc618
Przemek <przemek.7678@gmail.com>
2024-02-16 10:10:16 +0100

    CGAN

--- Model/GAN/model.py
+++ Model/GAN/model.py
@@ -1,9 +1,41 @@
 import tensorflow as tf
 
 
+def make_generator_c_model(num_classes, lattice_size = 32):
+    assert lattice_size % 4 == 0
+    hidden_size = lattice_size // 4
+    model = tf.keras.Sequential()
+
+    model.add(tf.keras.layers.Dense(hidden_size*hidden_size*256, use_bias=False, input_shape=(100 + num_classes,)))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
+
+    model.add(tf.keras.layers.Reshape((hidden_size, hidden_size, 256)))
+    assert model.output_shape == (None, hidden_size, hidden_size, 256)
+    
+    model.add(tf.keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None,  hidden_size,  hidden_size, 128)  
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
+
+    model.add(tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 2 * hidden_size, 2 * hidden_size, 64)  
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
+
+    model.add(tf.keras.layers.Conv2DTranspose(32, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 2 * hidden_size, 2 * hidden_size, 32)    
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
+
+    model.add(tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation=tf.sigmoid))
+    assert model.output_shape == (None, 4 * hidden_size, 4 * hidden_size, 1)
+    
+    return model
+
 def make_generator_model(lattice_size = 32):
     assert lattice_size % 4 == 0
-    hidden_size = lattice_size // 8
+    hidden_size = lattice_size // 4
     model = tf.keras.Sequential()
 
     model.add(tf.keras.layers.Dense(hidden_size*hidden_size*256, use_bias=False, input_shape=(100,)))
@@ -23,16 +55,15 @@ def make_generator_model(lattice_size = 32):
     model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.LeakyReLU())
 
-    model.add(tf.keras.layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 4 * hidden_size, 4 * hidden_size, 32)    
+    model.add(tf.keras.layers.Conv2DTranspose(32, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 2 * hidden_size, 2 * hidden_size, 32)    
     model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.LeakyReLU())
 
     model.add(tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation=tf.sigmoid))
-    assert model.output_shape == (None, 8 * hidden_size, 8 * hidden_size, 1)
+    assert model.output_shape == (None, 4 * hidden_size, 4 * hidden_size, 1)
     
     return model
-
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
@@ -66,9 +97,29 @@ def discriminator_loss(real_image, generated_image):
     return tf.reduce_mean(total_loss)
 
 
+def train_step_c(data, gen_loss_log, disc_loss_log, batch_size, noise_dim, generator, discriminator, generator_optimizer, discriminator_optimizer):
+      noise = tf.random.normal([batch_size, noise_dim])
+      images, temps = data
+      noise_t = tf.concat([noise, temps], axis=1)
+      with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise_t, training=True)
+      
+        real_output = discriminator(images, training=True)
+        generated_output = discriminator(generated_images, training=True)
+        gen_loss = generator_loss(generated_output)
+        gen_loss_log.append(gen_loss)
+        disc_loss = discriminator_loss(real_output, generated_output)
+        disc_loss_log.append(disc_loss)
+        
+      gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+      gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+      
+      generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+      discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
 def train_step(images, gen_loss_log, disc_loss_log, batch_size, noise_dim, generator, discriminator, generator_optimizer, discriminator_optimizer):
       noise = tf.random.normal([batch_size, noise_dim])
-      
+
       with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         generated_images = generator(noise, training=True)
       
