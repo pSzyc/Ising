b393fac209d79b639343cdc55e954c59310926ae
Przemek <przemek.7678@gmail.com>
2024-06-13 14:55:54 +0200

    gan plots

--- Plots/style.py
+++ Plots/style.py
@@ -5,21 +5,23 @@ from matplotlib import pyplot as plt
 #plt.rcParams['mathtext.fontset'] = 'custom'
 
 plt.rcParams.update({
-    'font.size': 12,
-    'axes.titlesize': 14,
-    'axes.labelsize': 12,
+    'font.size': 15,
+    'axes.titlesize': 15,
+    'axes.labelsize': 15,
     'xtick.labelsize': 10,
     'ytick.labelsize': 10,
-    'legend.fontsize': 10
+    'legend.fontsize': 15
 })
 
 def get_figsize(name: str) -> tuple:
     return fig_dict.get(name)
     
-half_fig = (3, 2)  # Adjust as needed for your layout
-figsize = (6, 4)  # Adjust as needed for your layout
+half_fig = (3, 3)  # Adjust as needed for your layout
+figsize = (7.5, 5)  # Adjust as needed for your layout
+long_fig = (4, 13)  # Adjust as needed for your layout
 
 fig_dict = {
     'fig': figsize,
-    'half_fig': half_fig
+    'half_fig': half_fig,
+    'long_fig': long_fig,
 }
